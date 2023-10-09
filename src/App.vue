1cf0e3b5a5be0c638efea6c52135a75dca792fc5
R-G2110 <argeldelacruz86@gmail.com>
2023-10-09 14:23:01 +0200

    [Init]: vue + vite

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+  export default {
+    name: 'App',
+    data(){
+      return {
+
+      }
+    }
+}
+</script>
+
+<template>
+  <h1>HELLO MOTO</h1>
+</template>
+
+
+
+<style>
+
+</style>
