e5cc6bd7cf661d29bf6cbeb27f55d15ee88a391c
R-G2110 <argeldelacruz86@gmail.com>
2023-10-09 15:07:24 +0200

    [Edit]: header in appTitle.vue

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,29 @@
+<script>
+	export default {
+		name: 'AppTitle',
+		data() {
+			return {
+				title: 'La mia prima app con Vite!'
+			}
+		}
+	}
+</script>
+
+<template>
+	<header>
+		<h1>{{ title }}</h1>
+	</header>
+</template>
+
+
+<style scoped>
+	header {
+		text-align: center;
+ 		position: fixed;
+  	z-index: 999;
+  	width: 100%;
+  	height: 70px;
+  	background-color: #FF6900;
+		color: white;
+	}
+</style>
